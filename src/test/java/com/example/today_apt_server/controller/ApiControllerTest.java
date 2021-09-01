86a6597a6680067c858abf5a5604698a7d9db4ec
Moomi98 <kymkjh2002@gmail.com>
2021-09-01 22:15:53 +0900

    2021.09.01 공공데이터 API로부터 요청 구현

--- src/test/java/com/example/today_apt_server/controller/ApiControllerTest.java
+++ src/test/java/com/example/today_apt_server/controller/ApiControllerTest.java
@@ -5,27 +5,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.yaml.snakeyaml.util.UriEncoder;
 
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLDecoder;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 public class ApiControllerTest {
 
     @Test
-    public void getOpenApiTest() throws UnsupportedEncodingException {
-        String serviceKey = "AAU8XVY6qAEr+jeoQWSx5+DtoZilWGKXT8jlz00LhC+nD51sqLyQcMnaT06waub/uy1OoEhGkIB4MXUpZ3qi9A==";
-        //String decodedServiceKey = URLDecoder.decode(serviceKey,"UTF-8");
-       // URI serviceKeyUri = UriComponentsBuilder.fromUriString(serviceKey).build().toUri();
-        String apiUri = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?LAWD_CD=11110&DEAL_YMD=201512&serviceKey=" + serviceKey;
-        URI uri = UriComponentsBuilder.fromUriString(apiUri).encode().build().toUri();
+    public void getOpenApiTest() throws UnsupportedEncodingException, URISyntaxException {
+        String encodedServiceKey = "AAU8XVY6qAEr%2BjeoQWSx5%2BDtoZilWGKXT8jlz00LhC%2BnD51sqLyQcMnaT06waub%2Fuy1OoEhGkIB4MXUpZ3qi9A%3D%3D";
+        String pageNo = "1";
+        String numOfRows = "10";
+        String LAWD_CD = "11110";
+        String DEAL_YMD = "201512";
+        String apiUri = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev";
+        String stringUri = UriComponentsBuilder.fromUriString(apiUri)
+                .queryParam("serviceKey", encodedServiceKey)
+                .queryParam("pageNo", pageNo)
+                .queryParam("numOfRows", numOfRows)
+                .queryParam("LAWD_CD", LAWD_CD)
+                .queryParam("DEAL_YMD", DEAL_YMD)
+                .build(false).toString();
+        URI uri = new URI(stringUri); // URI 클래스 사용 시 한 번 encoding 작업 후 get 요청
 
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
-        System.out.println(apiUri);
+        System.out.println(uri);
         System.out.println(result);
     }
 }
